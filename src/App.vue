c8221bc075f9d0ad278d39c86465cdea1a67949f
mingo <mingo233@qq.com>
2020-08-07 19:44:40 +0800

    vue案例初始化提交

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
+    <el-row>
+  <el-button icon="el-icon-search" circle></el-button>
+  <el-button type="primary" icon="el-icon-edit" circle></el-button>
+  <el-button type="success" icon="el-icon-check" circle></el-button>
+  <el-button type="info" icon="el-icon-message" circle></el-button>
+  <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+  <el-button type="danger" icon="el-icon-delete" circle></el-button>
+</el-row>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
